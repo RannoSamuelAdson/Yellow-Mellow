640c738d8c7ff290ac515fac16d6a5371298acbc
Markus Sulg <markus7sulg@gmail.com>
2025-10-04 12:27:46 +0300

    Player and main first iteration

--- YellowMellow/Assets/Scripts/Player.cs
+++ YellowMellow/Assets/Scripts/Player.cs
@@ -35,7 +35,7 @@ public class Player : MonoBehaviour
     void OnEnable() => inputActions.Enable();
     void OnDisable() => inputActions.Disable();
 
-    void Update()
+    void FixedUpdate()
     {
         HandleMovement();
     }
