c84c635ba31101f3ade6db819e46a21f42a99c6a
RannoSamuelAdson <116272456+RannoSamuelAdson@users.noreply.github.com>
2025-10-05 15:15:18 +0300

    did stuff

--- YellowMellow/Assets/Scripts/Sound Randomizer.cs
+++ YellowMellow/Assets/Scripts/Sound Randomizer.cs
@@ -12,7 +12,7 @@ public class SoundRandomizer : MonoBehaviour
     }
     public void PlayRandomSound()
     {
-        //Debug.Log("Playing sound");
+        Debug.Log("Playing sound");
         if (sounds.Length == 0) return;
 
         int index = Random.Range(0, sounds.Length);
